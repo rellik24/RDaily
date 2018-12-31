9ccc27da09eb227c95cf3f3c753fe818ec57732d
RellikPan <rellik@intrising.com.tw>
2018-12-31 14:20:03 +0800

    first commit

---
+++ app/pages/MainPage.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import styles from '../styles'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+      </View>
+    );
+  }
+}
+
